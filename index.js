c214083c7c6737f7f2a40c4c8e946c03882bf9f5
Cudby773 <josh.cudby1@gmail.com>
2019-02-07 19:50:55 +0000

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const readline = require('readline-sync');
 console.log('Welcome to the calculator, Josh!');
+console.log('Please enter some input:');
+const response = readline.prompt();
+num1 = response;
+num1 = +num1;
+console.log('Please enter some input:');
+const response2 = readline.prompt();
+num2 = response2;
+num2 = +num2;
+mult = num1*num2;
+console.log(mult)
