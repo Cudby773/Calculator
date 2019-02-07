24f509ad0b64d16684005a3b39559e4cbea21de6
Cudby773 <josh.cudby1@gmail.com>
2019-02-07 20:04:38 +0000

    4 operators using switch case

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const readline = require('readline-sync');
-console.log('Welcome to the calculator, Josh!');
+console.log('Welcome to the calculator!');
+console.log('--------------------------')
+console.log('Please enter an operator:');
+const operator = readline.prompt();
 console.log('Please enter some input:');
-const response = readline.prompt();
-num1 = response;
-num1 = +num1;
+const FirstArg = readline.prompt();
+const num1 = +FirstArg;
 console.log('Please enter some input:');
-const response2 = readline.prompt();
-num2 = response2;
-num2 = +num2;
-mult = num1*num2;
-console.log(mult)
+const SecondArg = readline.prompt();
+const num2 = +SecondArg;
+
+switch (operator) {
+    case '+':
+        sum = num1 + num2;
+        console.log('The answer is ' + sum);
+        break;
+    case '-':
+        diff = num1 - num2;
+        console.log('The answer is ' + diff);
+        break;
+    case '*':
+        mult = num1 * num2;
+        console.log('The answer is ' + mult);
+        break;
+    case '/':
+        ratio = num1 / num2;
+        console.log('The answer is ' + ratio);
+        break;
+    default:
+        console.log('This operator is not supported')
+}
